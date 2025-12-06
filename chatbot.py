886601059c426060bd54dedac4d60ec705064e3d
loukmanesavage <loukmanesavage@gmail.com>
2025-12-06 18:00:08 +0000

    10eme commit

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,5 @@
+#ce script sera utiliser pour lancer le serveur Flask
 from Backend import app
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- init_db.py
+++ init_db.py
@@ -1,5 +1,6 @@
+#Ici nous creeons les tables au niveau de PostgreSQL
 from Backend import app, db
 
 with app.app_context():
     db.create_all()
-    print("✅ Tables créées avec succès dans PostgreSQL!")
+    print("Les Tables ont créées avec succès")#après l'exécution ce massage va s'affiche pour montrer que tout s'est bien passer
